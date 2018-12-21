95e5df6b2e25f80adb7a8cfea8a5797debcd7912
SheinaT <Sheina.tolmas@gmail.com>
2018-12-21 08:37:19 +0200

    working

--- main.py
+++ main.py
@@ -20,13 +20,10 @@ def css(filepath):
 def img(filepath):
     return static_file(filepath, root="./images")
 
-
-
 @route('/')
 def index():
     sectionTemplate = "./templates/home.tpl"
     return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
-
 
 
 @error(404)
@@ -44,30 +41,59 @@ def index():
 
 @route('/ajax/show/<id>')
 def show_page(id):
-    sectionTemplate = './templates/browse.tpl'
     all_of_shows = [json.loads(utils.getJsonFromFile(utils.AVAILABE_SHOWS[i])) for i in range(12)]
     wanted_show = ""
     id = int(id)
     for show in all_of_shows:
         if show["id"] == id:
             wanted_show = show
-            return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate,
-                            sectionData=[wanted_show])
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate,
-                    sectionData=[wanted_show])
+    return template("./templates/show.tpl", version=utils.getVersion(),
+                    result=wanted_show)
+
 
 @route('/show/<id>')
 def show_page(id):
-    sectionTemplate = './templates/browse.tpl'
+    sectionTemplate= "./templates/show.tpl"
     all_of_shows = [json.loads(utils.getJsonFromFile(utils.AVAILABE_SHOWS[i])) for i in range(12)]
     wanted_show = ""
     id = int(id)
     for show in all_of_shows:
         if show["id"] == id:
             wanted_show = show
-            return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate,
-                            sectionData=[wanted_show])
     return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate,
-                    sectionData=[wanted_show])
+                    sectionData=wanted_show)
+
+
+@route('/ajax/show/<id>/episode/<episode>')
+def show_details(id, episode):
+    all_of_shows = [json.loads(utils.getJsonFromFile(utils.AVAILABE_SHOWS[i])) for i in range(12)]
+    id = int(id)
+    episode=int(episode)
+    for show in all_of_shows:
+        if show["id"] == id:
+            correct_show = show
+    for ep in correct_show["_embedded"]["episodes"]:
+        if ep["id"]==episode:
+            episode=ep
+    return template('./templates/episode.tpl', version=utils.getVersion(),
+                result=episode)
+
+
+@route('/show/<id>/episode/<episode>')
+def show_details(id, episode):
+    sectionTemplate = "./templates/episode.tpl"
+    all_of_shows = [json.loads(utils.getJsonFromFile(utils.AVAILABE_SHOWS[i])) for i in range(12)]
+    id = int(id)
+    episode=int(episode)
+    for show in all_of_shows:
+        if show["id"] == id:
+            correct_show = show
+    for ep in correct_show["_embedded"]["episodes"]:
+        if ep["id"]==episode:
+            episode=ep
+
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate,
+               sectionData=episode)
+
 
 run(host='localhost', port=os.environ.get('PORT', 7000))
